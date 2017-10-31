c257a680653ed83591dc57e3fa23d23eeaacb2e4
yhq <yhq72315@163.com>
2017-11-01 02:06:12 +0800

    Change Hello.java Addd Two Line

--- Hello.java
+++ Hello.java
@@ -2,6 +2,9 @@ public class Hello {
 
    public static void main(String args[]){
 
-      System.out.println("Hello GIT");
+      System.out.println("Hello Git");
+      System.out.println("www.mldn.cn");    
+      System.out.println("good job");  
    }
+    
 }
